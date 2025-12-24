9460d4d0c34a99c01f347af52f956e6d4473af09
libo1994qaq-bit <libo1994qaq@gmail.com>
2025-12-24 17:00:40 +0900

    HALIBO Beats
    Fighting

--- src/class/EachLine.ts
+++
@@ -1,9 +0,0 @@
-export class EachLine {
-  public beat: number
-  public rectangle: Phaser.GameObjects.Image
-
-  constructor(beat: number, rectangle: Phaser.GameObjects.Image) {
-    this.beat = beat
-    this.rectangle = rectangle
-  }
-}
--- src/class/Measure.ts
+++
@@ -1,9 +0,0 @@
-export class Measure {
-  public beat: number
-  public rectangle: Phaser.GameObjects.Rectangle
-
-  constructor(beat: number, rectangle: Phaser.GameObjects.Rectangle) {
-    this.beat = beat
-    this.rectangle = rectangle
-  }
-}
